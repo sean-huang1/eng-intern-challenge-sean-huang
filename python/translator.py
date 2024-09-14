bdee2161ba8d109d6d92fa1d695a4d66e6589931
sean-huang1 <sean.huang16@hotmail.com>
2024-09-14 15:27:19 -0400

    Updated translator.py

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,40 @@
+import sys
+
+BRAILLE_DICT = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..', 
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..', 
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.', 
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.', 
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO', 
+    'z': 'O..OOO', 
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
+    ' ': '......'
+}
+
+REVERSE_BRAILLE_DICT = {v: k for k, v in BRAILLE_DICT.items()}
+
+CAPITAL_MARKER = '.....O'
+
+def isBraille(s):
+    return all(c in 'O. ' for c in s)
+
+def englishToBraille(text):
+    braille = []
+    for char in text:
+        if char.isupper():
+            braille.append(CAPITAL_MARKER)
+            char = char.lower()
+        braille.append(BRAILLE_DICT.get(char, '......'))  
+    return ''.join(braille)
+
+input_text = sys.argv[1]
+if len(sys.argv) > 1:
+    if isBraille(sys.argv[1]):
+        print(englishToBraille(sys.argv[1])
+    else:
+        print("invalid input. Please try again")
+
+else:
+    print("error: no string provided. Please try again.")
 
